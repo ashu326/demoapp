e23cbe3e362fab16902cf0aacd71514a5ca78af1
ashu326h <ashutoshchauhan326@gmail.com>
2019-08-17 17:23:29 +0530

    initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,267 @@
+const crypto = require('crypto');
+
+const { validationResult } = require('express-validator/check');
+
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
+const User = require('../models/users');
+
+const bcrypt = require('bcrypt');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth:{
+    api_key: 'xyz'
+  }
+}))
+
+exports.getIndex = (req, res, next) => {
+  if(req.session.user) {
+    return res.redirect('/event');
+  }
+  let message = req.flash('error');
+  if(message.length > 0) {
+    message = message[0];
+  }
+  else {
+    message = null;
+  }
+  res.render('login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: req.session.isLoggedIn,
+    errorMessage: message
+  });
+};
+
+exports.getEvent = (req, res, next) => {
+  res.render('eventsform', {
+    path: '/event',
+    pageTitle: 'Event Query',
+    isAuthenticated: 'true'
+  })
+}
+
+exports.getEvents = (req, res, next) => {
+  res.render('events', {
+    path: '/events-list',
+    pageTitle: 'Events',
+    isAuthenticated: req.session.isLoggedIn
+  })
+}
+exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if(message.length > 0) {
+    message = message[0];
+  }
+  else {
+    message = null;
+  }
+  res.render('login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: req.session.isLoggedIn,
+    errorMessage: message
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash('signupError');
+  if(message.length > 0)
+  {
+    message = message[0];
+  }
+  else {
+    message = null;
+  }
+  res.render('signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: req.session.isLoggedIn,
+    errorMessage: message
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({email: email})
+    .then(user => {
+      if(!user) {
+        req.flash('error', 'Invalid email');
+        return res.redirect('/login');
+      }
+      bcrypt.compare(password, user.password)
+        .then(doMatch => {
+          if(doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/event');
+            });
+          }
+          req.flash('error', 'Invalid password');
+          res.redirect('/login'); 
+        })
+        .catch(err => {
+        console.log(err);
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
+
+  User.find({ email: email } )
+    .then(userDoc => {
+      if(userDoc.length > 0) {
+        req.flash('signupError', 'User already registered.');
+        return res.redirect('/signup');
+      }
+      return bcrypt.hash(password, 12)
+        .then(hashPassword => {
+          const user = new User({
+            email: email,
+            password: hashPassword,
+            cart: {items: []}
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'node-tutorial.com',
+            subject: "Signup successful!",
+            html: '<h1>Succefully signed up!</h1>'
+          });
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if(message.length > 0)
+  {
+    message = message[0];
+  }
+  else {
+    message = null;
+  }
+  res.render('reset', {
+    path: '/reset',
+    pageTitle: 'Password Reset',
+    errorMessage: message
+  });
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then(result => {
+        res.redirect('/');
+        transporter.sendMail({
+          to: req.body.email,
+          from: 'events@node-complete.com',
+          subject: 'Password reset',
+          html: `
+            <p>You requested a password reset</p>
+            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+          `
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then(user => {
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId
+  })
+    .then(user => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
---
+++ routes/auth.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { check, body } = require('express-validator/check');
+
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+router.get('/', authController.getIndex);
+
+router.get('/login', authController.getLogin);
+
+router.get('/signup', authController.getSignup);
+
+router.post('/login', authController.postLogin);
+
+router.post('/signup',  [
+    check('email', "Please enter a valid e-mail").isEmail().normalizeEmail(),
+    body('password', "Please use alpha numeric password").isLength({min: 5}).isAlphanumeric().trim(),
+    body('confirmPassword').trim().custom((value, {req}) => {
+        if(value !== req.body.password) {
+            throw new Error("password don't match");
+        }
+        return true;
+    })
+],authController.postSignup);
+
+router.post('/logout', authController.postLogout);
+
+router.get('/events-list', authController.getEvents);
+
+router.get('/event', authController.getEvent);
+
+router.get('/reset', authController.getReset); 
+
+router.post('/reset', authController.postReset);
+
+router.get('/reset/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
+
+module.exports = router;
